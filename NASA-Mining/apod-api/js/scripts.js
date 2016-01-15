6c8c74287c2d3d34715b0bfcdbd122f304399a8c
jbjoatton <jbjoatton@gmail.com>
2016-01-15 16:03:00 +0100

    Change JSON url

--- NASA-Mining/apod-api/js/scripts.js
+++ NASA-Mining/apod-api/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){ //quand le document est prêt
-    $.getJSON("https://api.nasa.gov/planetary/apod?api_key=WmBMhWaWxah92XV9zpZjqnWk6unLqawq7BlIGger&format=JSON")//je me connecte à l'API de la NASA avec ma clé personnelle
+    $.getJSON("https://api.nasa.gov/planetary/apod?api_key=WmBMhWaWxah92XV9zpZjqnWk6unLqawq7BlIGger")//je me connecte à l'API de la NASA avec ma clé personnelle
     .done(function(data){// quand les données sont récupérées
         console.log(data.title);// je les affiche dans la console
         $("#image-titre").html(data.title); // j'insère le titre entre les balises ayant l'ID #images-titre
@@ -11,4 +11,3 @@ $(document).ready(function(){ //quand le document est prêt
             .fadeIn(2000); // et l'affiche en fadeIn d'une durée de 2s
     });
 });
-
